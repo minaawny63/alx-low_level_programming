1b1a0f9b58acbe950cc103d799daf916549ffd36
minaawny63 <minaawny63@gmail.com>
2023-06-22 14:08:11 -0700

    s

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Doc
